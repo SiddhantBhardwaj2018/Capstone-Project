5dcf3de2da40ec56f8044059aa7d91c6f61dbb03
gvillamorales <gvillamorales@email.arizona.edu>
2021-09-29 19:57:17 -0700

    changed cred to cred1

--- api/api.py
+++ api/api.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred1 = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
