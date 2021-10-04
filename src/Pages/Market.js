87c1ae70dd785bce72e30d8dc1c747fc72574369
Xinyi Xiao <xiaoxinyi1688@163.com>
2021-10-03 19:33:01 -0700

    Build Market Table

--- src/Pages/Market.js
+++ src/Pages/Market.js
@@ -1,17 +1,50 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 
-function Market(){
-    const [Market,setMarket] = useState("")
+function MarketTable() {
+    let history = useHistory();
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [coins, setCoins] = useState([]);
+
     useEffect(() => {
-        fetch("/Market").then(res => res.json()).then(data => setMarket(data.Market))
-    },[])
+        fetch("/Market")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setCoins(data.Market);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
 
-    return (
-        <div>
-            <h1>{Market}</h1>
-        </div>
-    )
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <table>
+                {coins.map(coin => (
+                    <tr>
+                        <td><img src={coin.image} /></td>
+                        <td><h1>{coin.name}</h1><h5>{coin.symbol}</h5></td>
+                        <td>{coin.current_princ}</td>
+                        <td>{coin.price_change_24h}</td>
+                        <td>{coin.price_change_percentage_24h}</td>
+                        <td><button onClick={() => {
+                            history.push('/Information');
+                        }}> Detail </button></td>
+                    </tr>
+                ))}
+            </table>
+        );
+    }
 }
 
-export default Market;
+export default MarketTable;
