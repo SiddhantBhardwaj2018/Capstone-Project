4935934941049f4a0b8cb6b04d79050bb1f68bdf
jtrax22 <jtraxler18@gmail.com>
2021-10-05 18:26:43 -0700

    test

--- api/Information.py
+++ api/Information.py
@@ -1,5 +1,6 @@
 from pycoingecko import CoinGeckoAPI
 
 #This is the Information.py file 
+#testing github
 cg = CoinGeckoAPI()
 print(len(cg.get_price(ids= ['Cardano', 'Algorand', 'Cosmos', 'TRON', 'Tezos', 'EOS', 'Orbs', 'Waves', 'THORChain', 'Decred', 'ICON', 'Qtum', 'Ontology', 'Lisk', 'Ardor', 'Plex', 'Function X', 'Ark', 'TomoChain', 'Tenset', 'Steem', 'Wanchain', 'BitShares', 'Oasis Network', 'PIVX', 'Nuls', 'Verasity', 'DUSK Network', 'Navcoin', 'Neblio', 'Stakenet', 'Million', 'Peercoin', 'NXT', 'Stakecube', 'DataHighway', 'Lanceria', 'Blocknet', 'BlackCoin', 'VAULT', 'NIX', 'Tokenize Xchange', 'CREED', 'Nexalt', 'Hydra', 'Bitball Treasure', 'RAKON', 'Trading Pool Coin', 'GICTrade', 'MONGO Coin'], vs_currencies='usd')))
