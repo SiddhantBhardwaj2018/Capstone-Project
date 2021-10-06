83b19a0458052581ce89246dda1d10f52333e8c2
gvillamorales <gvillamorales@email.arizona.edu>
2021-10-06 15:59:25 -0700

    Imported firebase_admin to my local repository

--- api/SignIn.py
+++ api/SignIn.py
@@ -1,3 +1,16 @@
+from flask import Flask, request
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+user_accounts = db.collection("user_account_table")
+app = Flask(__name__)
+
 #This is the back-end implementation for SignIn Page
 def process_login(username, password):
-    return "This works hurray!"
+    return "aye"
