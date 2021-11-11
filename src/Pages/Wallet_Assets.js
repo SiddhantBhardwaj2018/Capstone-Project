94d8d6a4902a0d617b947525b9b84258d8028e77
gvillamorales <gvillamorales@email.arizona.edu>
2021-11-10 19:51:42 -0700

    Wallet_Assets is made

---
+++ src/Pages/Wallet_Assets.js
@@ -0,0 +1,28 @@
+
+function updateGetPrice({ coins,CoinIds,setPortfolio,setlastPortfolio }){
+    let string = "";
+    let i = 0;
+    console.log(coins)
+    for (const coin in coins) {
+      if (i === 0) {
+        string += CoinIds[coin];
+      } else {
+        string += "%2C" + CoinIds[coin];
+      }
+      i++;
+    }
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${string}&order=market_cap_desc&per_page=${
+        Object.keys(coins).length
+      }&page=1&sparkline=false`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        cp_list = []
+        for (let index = 0; index < data.length; index++) {
+          cp_list.append(data[index].current_price);
+        }
+        //return 
+      });
+  };
